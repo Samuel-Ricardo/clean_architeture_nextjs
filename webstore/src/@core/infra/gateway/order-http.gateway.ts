c51414486754aaa61e1a87a0eb3476d139994e77
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-07-18 17:17:20 -0300

    [:sparkles:] | implements: gateway - order > http (infra)

--- webstore/src/@core/infra/gateway/order-http.gateway.ts
+++ webstore/src/@core/infra/gateway/order-http.gateway.ts
@@ -1,4 +1,5 @@
 import { Order } from "@/@core/domain/entities/order";
+import { Product } from "@/@core/domain/entities/product";
 import { OrderGateway } from "@Core/domain/gateway/order.gateway";
 import { AxiosInstance } from "axios";
 
@@ -15,7 +16,16 @@ export class OrderHttpGateway  implements OrderGateway {
         });
     }
     findById(id: number): Promise<Order> {
-        throw new Error("Method not implemented.");
+      return this
+        .http
+        .get(`/orders/${id}`)
+        .then(res => new Order({
+          id: res.data.id,
+          credit_card_number: res.data.credit_card_number,
+          products: res.data.products.map((product: any) => new Product(
+            {id: product.id, name: product.name, price: product.price, description: product.description}
+          ))
+      }))
     }
 
 }
