4267a87a71d05c2c59656004644c82b09bc9d488
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-07-18 15:43:51 -0300

    [:sparkles:]  | create: entity - order > getters (module)

--- webstore/src/@core/domain/entities/order.ts
+++ webstore/src/@core/domain/entities/order.ts
@@ -11,5 +11,24 @@ export class Order {
   constructor(public props: OrderProps) {}
 
 
+  get id() {
+    return this.props.id;
+  }
+
+  get products() {
+    return this.props.products;
+  }
+
+  get credit_card_number() {
+    return this.props.credit_card_number;
+  }
+
+  get total() {
+    return this.props.products.reduce(
+      (total, product) => total + product.price,
+      0
+    );
+  }
+
 
 }
