72f30d506e6e76a0f7dde5cae4f4d2af5706f227
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-07-18 15:27:46 -0300

    [:sparkles:]  | create: entity - cart > getters (module)

--- webstore/src/@core/domain/entities/carts.ts
+++ webstore/src/@core/domain/entities/carts.ts
@@ -8,4 +8,12 @@ export class Cart {
 
   constructor(public props: CartProps) {}
 
+  get total() {
+    return this.props.products.reduce((total, product) => total + product.price, 0)
+  }
+
+  get products() {
+    return this.props.products;
+  }
+
 }
