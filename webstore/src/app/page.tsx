9569b1a74e5363a3484835960075676085d54667
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-07-19 15:47:31 -0300

    [:sparkles:]  | create: page - home (app)

--- webstore/src/app/page.tsx
+++ webstore/src/app/page.tsx
@@ -1,18 +1,37 @@
+import { ListProductsUseCase } from '@/@core/application/product/list-products.use-case'
 import { CartProvider } from './context/cart.provider'
 import { Product, ProductProps } from '@/@core/domain/entities/product'
-import { NextPage } from 'next'
+import { Registry, container } from '@/@core/infra/container-registry'
+import { GetServerSideProps, NextPage } from 'next'
+import Link from "next/link";
 
 export type HomeProps = {
   products: ProductProps[]
 }
 
-const Home: NextPage<HomeProps> = ({products}) => (  
+const Home: NextPage = async () => {  
 
+  const useCase = container.get<ListProductsUseCase>(Registry.ListProductUseCase);
+  const products = (await useCase.execute())
+    
+  
+
+return (
   <CartProvider> 
     <div>
-      Hello World!
+      <h1>E-Commerce :D</h1>
+        <ul>
+          {products.map((product, key) => (
+            <li key={key}>
+              <label>Nome: </label> {product.name} | 
+              <Link href={`/products/${product.id}`} passHref>
+                Ver
+              </Link>
+            </li>
+          ))}
+        </ul>
     </div>
   </CartProvider>
-)
+)}
 
 export default Home;
