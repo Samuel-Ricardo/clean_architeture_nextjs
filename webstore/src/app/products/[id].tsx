561684b5f28440ae5e9a25b7ac07232c740601fa
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-07-19 15:59:51 -0300

    [:sparkles:]  | create: page - product details (app)

--- webstore/src/app/products/[id].tsx
+++ webstore/src/app/products/[id].tsx
@@ -1,6 +1,33 @@
-import { ProductProps } from "@Core/domain/entities/product"
+import { Product, ProductProps } from "@Core/domain/entities/product"
+import { NextPage } from "next";
+import { useContext } from "react";
+import { CartContext } from "../context/cart.provider";
 
 export type PropductDetailProps = {
   product: ProductProps;
 };
+
+export const PropductDetail:NextPage<PropductDetailProps> = ({ product }: PropductDetailProps) => {
+  
+  const entity = new Product({...product});
+  const cartContext = useContext(CartContext);
+
+  return (
+    <div>
+      <h3>
+        {entity.name}
+      </h3>
+
+      <label> Preço: </label> {entity.price}
+
+      <button onClick={() => cartContext.addProduct(entity)}> 
+        Adicionar no carrinho
+      </button>
+    </div>
+  )
+};
+
+export default PropductDetail;
+
+
 
