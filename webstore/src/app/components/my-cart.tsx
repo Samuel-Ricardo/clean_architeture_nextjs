2a0db41bad0ec03cb7ebd9ff10fc73628fc9064c
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-07-19 14:22:27 -0300

    [:sparkles:]  | create: component - my-cart (app)

---
+++ webstore/src/app/components/my-cart.tsx
@@ -0,0 +1,18 @@
+import { PropsWithChildren, useContext } from "react";
+import { CartContext } from "../context/cart.provider";
+import Link from "next/link";
+
+export const MyCart = (props: PropsWithChildren) => {
+  const cartContext = useContext(CartContext);
+  
+  return (
+    <nav>
+      Cart - Total {cartContext.cart.total} | Items{" "}
+      {cartContext.cart.products.length} |{" "}
+      <Link href="/checkout" passHref>
+        <a href="">Finalizar Compra</a>
+      </Link>
+    </nav>
+  )
+}
+
