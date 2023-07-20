0646b70fe1b7505f6e66dae10e2dbdb68ca1efbd
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-07-20 13:05:56 -0300

    [:sparkles:]  | create: page - checkout success (app)

--- webstore/src/app/checkout/[id]/success/page.tsx
+++ webstore/src/app/checkout/[id]/success/page.tsx
@@ -1,5 +1,24 @@
 import { Order } from "@Core/domain/entities/order";
+import { NextPage } from "next";
 
 export type CheckoutSuccessPageProps = {
   order: Order;
 };
+
+
+export function CheckoutSuccessPage ({order}: CheckoutSuccessPageProps) {
+
+
+  return (
+    <div>
+      <h3>Parabéns Sua compra {order.id} foi efetivada</h3>
+      <ul>
+        {order.products.map( item => (
+          <li key={item.id}>
+            Produto {item.name} - {item.price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
